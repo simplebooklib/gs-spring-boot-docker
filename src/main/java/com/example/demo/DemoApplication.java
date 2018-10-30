21be7d879575e6122817dddf699c6eee13a891c0
kapentaz <kapentaz@daum.net>
2018-10-30 09:26:38 +0900

    init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World2";
+        return "Hello Docker World3";
     }
 
     public static void main(String[] args) {
